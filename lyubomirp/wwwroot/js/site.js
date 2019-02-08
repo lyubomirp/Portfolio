51980b9c9750edc203bf269b139e49534ff824c0
lyubomirp <lpetkov44@yahoo.com>
2019-02-08 03:32:24 +0200

    Main source for when site went live

---
+++ lyubomirp/wwwroot/js/site.js
@@ -0,0 +1,12 @@
+﻿/* particlesJS.load(@dom-id, @path-json, @callback (optional)); */
+particlesJS.load('particles-js', "js/particlesjs-config.json", function () {
+    console.log('callback - particles.js config loaded');
+});
+
+
+
+
+
+
+
+
